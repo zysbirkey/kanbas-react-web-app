16f8de2b908a7aaf9877b6263fa825055b4b3919
Ysanne <136540589+zysbirkey@users.noreply.github.com>
2024-10-03 23:30:11 -0400

    a2 CSS and Bootstrap

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -6,8 +6,6 @@ import { IoSettingsOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 export default function KanbasNavigation() {
   return (
-    /* add list-group class     */
-
     <div id="wd-kanbas-navigation" style={{ width: 120 }} 
          className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
       <a id="wd-neu-link" target="_blank" 
@@ -15,23 +13,21 @@ export default function KanbasNavigation() {
         rel="noopener noreferrer" 
         className="list-group-item bg-black border-0 text-center">
         <img src="/images/NEU.png" width="75px" alt="Northeastern University Logo" /></a><br />
-
       <Link to="/Kanbas/Account" id="wd-account-link"
         className="list-group-item text-center border-0 bg-black text-white" rel="noreferrer">
         <FaRegCircleUser className="fs-1 text text-white" /><br />
         Account </Link><br />
-
       <Link to="/Kanbas/Dashboard" id="wd-dashboard-link"
         className="list-group-item text-center border-0
                    bg-white text-danger">
         <AiOutlineDashboard className="fs-1 text-danger" /><br />
         Dashboard </Link><br />
-
       <Link to="/Kanbas/Courses/" id="wd-course-link"
         className="list-group-item text-white
                    bg-black text-center border-0">
         <LiaBookSolid className="fs-1 text-danger" /><br />
         Courses </Link><br />
+      {/* complete styling the rest of the links */}
 
       <Link to="/Kanbas/Calendar" id="wd-calendar-link"
         className="list-group-item text-white
