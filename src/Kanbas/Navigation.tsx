e11f6bb23bf7fe9eb1cf39ba95fc8e73eef72207
Ysanne <136540589+zysbirkey@users.noreply.github.com>
2024-10-17 21:54:34 -0400

    a3 Netlify debug1

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -2,7 +2,7 @@ import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
-import { IoSettingsOutline } from "react-icons/io5";
+// import { IoSettingsOutline } from "react-icons/io5";
 import { Link, useLocation } from "react-router-dom";
 export default function KanbasNavigation() {
   const { pathname } = useLocation();
