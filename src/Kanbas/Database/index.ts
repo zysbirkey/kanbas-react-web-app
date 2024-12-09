4789b7b7af6eba65b9e3e5afc06618f1543f3dfd
Ysanne <136540589+zysbirkey@users.noreply.github.com>
2024-12-08 23:46:19 -0500

    a6

--- src/Kanbas/Database/index.ts
+++ src/Kanbas/Database/index.ts
@@ -2,5 +2,4 @@ import courses from "./courses.json";
 import modules from "./modules.json";
 import assignments from "./assignments.json";
 import users from "./users.json";
-import enrollments from "./enrollments.json";
-export { courses, modules, assignments, users, enrollments};
+export { courses, modules, assignments, users};
